7b2cd78079a1b7f95aba53f572033721eeeb24f3
Vitor <vitor-27@hotmail.com>
2021-12-19 20:14:14 -0300

    Create repository to deal with users table. Started with signup functionality

---
+++ src/main/kotlin/com/study/ktor/repository/UsersRepository.kt
@@ -0,0 +1,53 @@
+package com.study.ktor.repository
+
+import com.study.ktor.plugins.exception.SignupFailedException
+import com.study.ktor.plugins.exception.UserAlreadySignedUpException
+import com.study.ktor.plugins.route.model.UserSignup
+import com.study.ktor.repository.configuration.DRIVER
+import com.study.ktor.repository.configuration.PASSWORD
+import com.study.ktor.repository.configuration.URL
+import com.study.ktor.repository.configuration.USER_DATABASE
+import com.study.ktor.repository.model.User
+import com.study.ktor.repository.table.Users
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object UsersRepository {
+    init {
+        Database.connect(url = URL, driver = DRIVER, user = USER_DATABASE, password = PASSWORD)
+        transaction {
+            SchemaUtils.create(Users)
+        }
+    }
+
+    fun getAllUsers(): List<User> = transaction {
+        Users.selectAll().map { Users.toUser(it) }
+    }
+
+    fun signupUser(userSignup: UserSignup) {
+        val isUserAlreadySignedUp = isUserAlreadySignedUp(userSignup.email)
+
+        if (isUserAlreadySignedUp) {
+            throw UserAlreadySignedUpException
+        } else {
+            try {
+                transaction {
+                    Users.insertAndGetId {
+                        it[firstName] = userSignup.firstName
+                        it[secondName] = userSignup.secondName
+                        it[email] = userSignup.email
+                        it[password] = userSignup.password
+                    }
+                }
+            } catch (exception: IllegalStateException) {
+                throw SignupFailedException
+            }
+        }
+    }
+
+    private fun isUserAlreadySignedUp(email: String): Boolean = transaction {
+        !Users.select {
+            Users.email eq email
+        }.limit(1).empty()
+    }
+}
