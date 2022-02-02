e581635557ef529f720e2b0fb64e479342edebb7
vitor-aoki <vitorkaoki@gmail.com>
2022-02-02 12:23:37 -0300

    Create UserSession object to respond signup

---
+++ src/main/kotlin/com/study/ktor/plugins/route/model/UserSession.kt
@@ -0,0 +1,11 @@
+package com.study.ktor.plugins.route.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserSession(
+    @SerialName("user_id") val userId: String,
+    val token: String,
+    @SerialName("expires_at") val expiresAt: Long
+)
