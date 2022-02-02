30df903dcfb367a47da5a3358cc76b6c64374c88
vitor-aoki <vitorkaoki@gmail.com>
2022-02-02 12:25:16 -0300

    Remove signup and session functions from UsersRouting

--- src/main/kotlin/com/study/ktor/plugins/route/UsersRouting.kt
+++ src/main/kotlin/com/study/ktor/plugins/route/UsersRouting.kt
@@ -1,16 +1,9 @@
 package com.study.ktor.plugins.route
 
-import com.study.ktor.plugins.result.UserSignupResult
-import com.study.ktor.plugins.route.model.UserSignup
-import com.study.ktor.repository.SessionsRepository
 import com.study.ktor.repository.UsersRepository
 import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.util.pipeline.*
-import kotlinx.serialization.SerializationException
 
 private const val USER_PATH = "/user"
 private const val ALL_PATH = "all"
@@ -28,27 +21,6 @@ fun Route.usersRoute() {
         post(SIGNUP_USER_PATH) {
             signup()
         }
-    }
-}
-
-private suspend fun PipelineContext<Unit, ApplicationCall>.signup() {
-    try {
-        val userSignup = call.receive<UserSignup>()
-        val result = UsersRepository.signupUser(userSignup)
-        handleSignupResult(result)
-    } catch (exception: SerializationException) {
-        wrongUserSignupBody()
-    }
-}
-
-private suspend fun PipelineContext<Unit, ApplicationCall>.handleSignupResult(result: UserSignupResult) {
-    when (result) {
-        is UserSignupResult.UserSignupSuccess -> {
-            SessionsRepository.createSession(result.id)
-            call.respond(HttpStatusCode.Created)
-        }
-        is UserSignupResult.UserAlreadySignedUp -> userAlreadySignedUp()
-        is UserSignupResult.SignupFailed -> signupFailed()
     }
 }
 
