e74db9172e9903b4932a63392cdec44ac745a2d9
vitor-aoki <vitorkaoki@gmail.com>
2022-02-06 05:50:07 -0300

    Add function to validate password

--- src/main/kotlin/com/study/ktor/repository/model/User.kt
+++ src/main/kotlin/com/study/ktor/repository/model/User.kt
@@ -1,9 +1,13 @@
 package com.study.ktor.repository.model
 
+import com.study.ktor.repository.Cryptography
+
 data class User(
     val id: Int,
     val firstName: String,
     val secondName: String,
     val email: String,
     val password: String
-)
+) {
+    fun isPasswordCorrect(receivedPassword: String): Boolean = password == Cryptography.sha512(receivedPassword)
+}
