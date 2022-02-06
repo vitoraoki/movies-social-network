0f6376da412132cae908ead0be882fbb30f441a8
vitor-aoki <vitorkaoki@gmail.com>
2022-02-06 05:52:51 -0300

    Update SessionRouting to handle update results

--- src/main/kotlin/com/study/ktor/plugins/route/session/SessionRouting.kt
+++ src/main/kotlin/com/study/ktor/plugins/route/session/SessionRouting.kt
@@ -1,31 +1,57 @@
 package com.study.ktor.plugins.route.session
 
-import com.study.ktor.plugins.route.session.result.SessionResult
 import com.study.ktor.plugins.route.session.model.UserSession
-import com.study.ktor.plugins.route.signup.error.signupFailed
+import com.study.ktor.plugins.route.session.result.SessionResult
+import com.study.ktor.plugins.route.signin.error.signInFailed
+import com.study.ktor.plugins.route.signup.error.signUpFailed
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.util.pipeline.*
 
-suspend fun PipelineContext<Unit, ApplicationCall>.handleCreateSessionResult(
+suspend fun PipelineContext<Unit, ApplicationCall>.handleSessionResult(
     userId: Int,
-    createSessionResult: SessionResult
+    sessionResult: SessionResult
 ) {
-    when(createSessionResult) {
-        is SessionResult.CreateSessionSuccess -> handleCreateSessionSuccess(userId, createSessionResult)
-        is SessionResult.CreateSessionFailed -> signupFailed()
+    when(sessionResult) {
+        is SessionResult.CreateSessionSuccess -> handleCreateSessionSuccess(userId, sessionResult)
+        is SessionResult.CreateSessionFailed -> signUpFailed()
+        is SessionResult.UpdateSessionSuccess -> handleUpdateSessionSuccess(userId, sessionResult)
+        is SessionResult.UpdateSessionFailed -> signInFailed()
     }
+}
+
+private suspend fun PipelineContext<Unit, ApplicationCall>.handleUpdateSessionSuccess(
+    userId: Int,
+    updateSessionSuccess: SessionResult.UpdateSessionSuccess
+) {
+    handleSessionSuccess(
+        userId = userId,
+        token = updateSessionSuccess.token,
+        expiresAt = updateSessionSuccess.expiresAt
+    )
 }
 
 private suspend fun PipelineContext<Unit, ApplicationCall>.handleCreateSessionSuccess(
     userId: Int,
     createSessionResult: SessionResult.CreateSessionSuccess
 ) {
-    val userSession = UserSession(
-        userId = userId.toString(),
+    handleSessionSuccess(
+        userId = userId,
         token = createSessionResult.token,
         expiresAt = createSessionResult.expiresAt
+    )
+}
+
+private suspend fun PipelineContext<Unit, ApplicationCall>.handleSessionSuccess(
+    userId: Int,
+    token: String,
+    expiresAt: Long
+) {
+    val userSession = UserSession(
+        userId = userId.toString(),
+        token = token,
+        expiresAt = expiresAt
     )
     call.respond(status = HttpStatusCode.Created, message = userSession)
 }
