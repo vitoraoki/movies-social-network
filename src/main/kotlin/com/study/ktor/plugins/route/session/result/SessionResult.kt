dfd9f6dac589e5bd4ec83e3eff5a81746a350b56
vitor-aoki <vitorkaoki@gmail.com>
2022-02-07 01:18:19 -0300

    Update SessionResult to have result for session validation

--- src/main/kotlin/com/study/ktor/plugins/route/session/result/SessionResult.kt
+++ src/main/kotlin/com/study/ktor/plugins/route/session/result/SessionResult.kt
@@ -1,5 +1,7 @@
 package com.study.ktor.plugins.route.session.result
 
+import com.study.ktor.plugins.route.ErrorMessages
+
 sealed class SessionResult {
     data class CreateSessionSuccess(
         val token: String,
@@ -11,6 +13,20 @@ sealed class SessionResult {
         val expiresAt: Long
     ): SessionResult()
 
+    object ValidSession: SessionResult()
+
     object CreateSessionFailed: SessionResult()
     object UpdateSessionFailed: SessionResult()
+
+    data class SessionExpired(
+        val message: String = ErrorMessages.SESSION_EXPIRED.value
+    ): SessionResult()
+
+    data class InvalidToken(
+        val message: String = ErrorMessages.INVALID_TOKEN.value
+    ): SessionResult()
+
+    data class SessionNotFound(
+        val message: String = ErrorMessages.SESSION_NOT_FOUND.value
+    ): SessionResult()
 }
