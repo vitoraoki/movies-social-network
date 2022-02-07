89d37bd41dff6f7cfce2347fc1e3e7ec73c3581f
vitor-aoki <vitorkaoki@gmail.com>
2022-02-07 01:16:15 -0300

    Update Sessions table object to cast resultRow to Session object

--- src/main/kotlin/com/study/ktor/repository/table/Sessions.kt
+++ src/main/kotlin/com/study/ktor/repository/table/Sessions.kt
@@ -1,7 +1,9 @@
 package com.study.ktor.repository.table
 
+import com.study.ktor.repository.model.Session
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ResultRow
 
 private const val TABLE_NAME = "Sessions"
 private const val COLUMN_USER_ID = "user_id"
@@ -12,4 +14,11 @@ object Sessions: IntIdTable(TABLE_NAME) {
     val userId: Column<Int> = integer(COLUMN_USER_ID)
     val token: Column<String> = text(TOKEN)
     val expiresAt: Column<Long> = long(EXPIRES_AT)
+
+    fun toSession(resultRow: ResultRow): Session = Session(
+        id = resultRow[id].value,
+        userId = resultRow[userId],
+        token = resultRow[token],
+        expiresAt = resultRow[expiresAt]
+    )
 }
