27d0fad60d4fa8dae36d8eedfa123926fb66ce72
vitor-aoki <vitorkaoki@gmail.com>
2022-02-07 01:20:24 -0300

    Add route to validate users's session

--- src/main/kotlin/com/study/ktor/plugins/route/UsersRouting.kt
+++ src/main/kotlin/com/study/ktor/plugins/route/UsersRouting.kt
@@ -1,5 +1,6 @@
 package com.study.ktor.plugins.route
 
+import com.study.ktor.plugins.route.session.validateSession
 import com.study.ktor.plugins.route.signin.signIn
 import com.study.ktor.plugins.route.signup.signUp
 import com.study.ktor.repository.UsersRepository
@@ -11,6 +12,8 @@ private const val USER_PATH = "/user"
 private const val ALL_PATH = "all"
 private const val SIGN_UP_USER_PATH = "/sign_up"
 private const val SIGN_IN_USER_PATH = "/sign_in"
+private const val SESSION = "/session"
+private const val SESSION_VALIDATE = "$SESSION/validate"
 
 fun Route.usersRoute() {
     route(USER_PATH) {
@@ -25,6 +28,10 @@ fun Route.usersRoute() {
         
         post(SIGN_IN_USER_PATH) {
             signIn()
+        }
+
+        post(SESSION_VALIDATE) {
+            validateSession()
         }
     }
 }
--- src/main/kotlin/com/study/ktor/plugins/route/session/SessionRouting.kt
+++ src/main/kotlin/com/study/ktor/plugins/route/session/SessionRouting.kt
@@ -1,13 +1,25 @@
 package com.study.ktor.plugins.route.session
 
-import com.study.ktor.plugins.route.session.model.UserSession
+import com.study.ktor.plugins.route.session.error.invalidToken
+import com.study.ktor.plugins.route.session.error.sessionExpired
+import com.study.ktor.plugins.route.session.error.sessionNotFound
+import com.study.ktor.plugins.route.session.model.SessionResponse
+import com.study.ktor.plugins.route.session.model.SessionValidateData
 import com.study.ktor.plugins.route.session.result.SessionResult
 import com.study.ktor.plugins.route.signin.error.signInFailed
 import com.study.ktor.plugins.route.signup.error.signUpFailed
+import com.study.ktor.repository.SessionsRepository
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.util.pipeline.*
+
+suspend fun PipelineContext<Unit, ApplicationCall>.validateSession() {
+    val sessionValidateData = call.receive<SessionValidateData>()
+    val result = SessionsRepository.validateSession(sessionValidateData = sessionValidateData)
+    handleSessionResult(userId = sessionValidateData.userId, sessionResult = result)
+}
 
 suspend fun PipelineContext<Unit, ApplicationCall>.handleSessionResult(
     userId: Int,
@@ -18,7 +30,15 @@ suspend fun PipelineContext<Unit, ApplicationCall>.handleSessionResult(
         is SessionResult.CreateSessionFailed -> signUpFailed()
         is SessionResult.UpdateSessionSuccess -> handleUpdateSessionSuccess(userId, sessionResult)
         is SessionResult.UpdateSessionFailed -> signInFailed()
+        is SessionResult.ValidSession -> handleValidSession()
+        is SessionResult.SessionExpired -> sessionExpired(sessionExpired = sessionResult)
+        is SessionResult.InvalidToken -> invalidToken(invalidToken = sessionResult)
+        is SessionResult.SessionNotFound -> sessionNotFound(sessionNotFound = sessionResult)
     }
+}
+
+private suspend fun PipelineContext<Unit, ApplicationCall>.handleValidSession() {
+    call.respond(HttpStatusCode.OK)
 }
 
 private suspend fun PipelineContext<Unit, ApplicationCall>.handleUpdateSessionSuccess(
@@ -48,10 +68,10 @@ private suspend fun PipelineContext<Unit, ApplicationCall>.handleSessionSuccess(
     token: String,
     expiresAt: Long
 ) {
-    val userSession = UserSession(
-        userId = userId.toString(),
+    val sessionResponse = SessionResponse(
+        userId = userId,
         token = token,
         expiresAt = expiresAt
     )
-    call.respond(status = HttpStatusCode.Created, message = userSession)
+    call.respond(status = HttpStatusCode.Created, message = sessionResponse)
 }
