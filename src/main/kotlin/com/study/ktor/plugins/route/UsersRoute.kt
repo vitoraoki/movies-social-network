eff57d888305de0cb80a4553b9162d4e63b8f8bd
vitoraoki <vitorkaoki@gmail.com>
2022-03-29 22:33:40 -0300

    Remove getAll users endpoint

--- src/main/kotlin/com/study/ktor/plugins/route/UsersRoute.kt
+++ src/main/kotlin/com/study/ktor/plugins/route/UsersRoute.kt
@@ -3,13 +3,9 @@ package com.study.ktor.plugins.route
 import com.study.ktor.plugins.route.session.validateSession
 import com.study.ktor.plugins.route.signin.signIn
 import com.study.ktor.plugins.route.signup.signUp
-import com.study.ktor.repository.UsersRepository
-import io.ktor.application.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 private const val USER_PATH = "/user"
-private const val ALL_PATH = "all"
 private const val SIGN_UP_USER_PATH = "/sign_up"
 private const val SIGN_IN_USER_PATH = "/sign_in"
 private const val SESSION = "/session"
@@ -17,15 +13,10 @@ private const val SESSION_VALIDATE = "$SESSION/validate"
 
 fun Route.usersRoute() {
     route(USER_PATH) {
-        get(ALL_PATH) {
-            val users = UsersRepository.getAllUsers()
-            call.respond(users)
-        }
-
         post(SIGN_UP_USER_PATH) {
             signUp()
         }
-        
+
         post(SIGN_IN_USER_PATH) {
             signIn()
         }
